065b0cb82dc8c37dcaacb243ba4aa4b3dec57dc3
Kanishk Gupta <kanishkgupta1234@gmail.com>
2021-07-04 11:57:17 +0530

    feat: Added add to cart feature

---
+++ src/components/Products.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import {products} from "../assets/products";
+
+export default function Products(){
+    const [showProducts,setProducts]=useState(products);
+    const [cartItems,setCartItems]=useState([]);
+
+    useEffect(()=>{
+        const cart=JSON.parse(localStorage.getItem("cart"));
+        setCartItems(cart??[]);
+    },[])
+
+    useEffect(()=>{
+        localStorage.setItem("cart",JSON.stringify(cartItems));
+    },[cartItems])
+
+    const addToCart=(item)=>{
+        console.log("added")
+        const tempCart=cartItems;
+        tempCart.push({...item,count:1});
+        setCartItems([...tempCart])
+    }
+
+    const removeFromCart=(itemParam)=>{
+        let tempCart=cartItems;
+        tempCart=tempCart.filter((item)=>item.id!==itemParam.id);
+        setCartItems([...tempCart])
+    }
+
+    const itemPresent=(item)=>{
+        let isPresent=false;
+        cartItems.forEach(({id})=>{
+            if(id===item.id)
+            isPresent=true
+        })
+        return isPresent
+    }
+
+    return (
+        <>
+        Produts
+        <div className="products-container">
+            {
+                showProducts.map(({id,title,img,attributes},index,arr)=>(
+                    <div className="product-card">
+                    <img src={img} alt="image"/>
+                    <span>{title}</span>
+                    <span>Price: {attributes.price}</span><br/>
+                    
+                        {
+                        itemPresent(arr[index])?
+                        <button onClick={()=>removeFromCart(arr[index])}>Remove from cart</button>
+                        :
+                        <button onClick={()=>addToCart(arr[index])}>Add to cart</button>}
+                    </div>
+                ))
+            }
+        </div>
+        </>
+    )
+}
